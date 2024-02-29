07790bd66e8625eea328b64cc17d90dbd961927b
Martin Ramonda <tini.ramonda@gmail.com>
2024-02-29 20:43:25 +0100

    document dao implemented

--- src/main/java/db/DocumentDataLoader.java
+++ src/main/java/db/DocumentDataLoader.java
@@ -1,9 +1,12 @@
 package db;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import java.util.ArrayList;
 import java.util.List;
 import model.User;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 /**
@@ -27,30 +30,54 @@ public class DocumentDataLoader {
     }
     
     public boolean addUser(User user){
-    
+        try{
+            Document userDoc = new Document();
+            userDoc.append("_id", new ObjectId())
+                    .append("username", user.getUsername())
+                    .append("edad", user.getEdad())
+                    .append("shows", user.getShows());
+            documentCollection.insertOne(userDoc);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+
     }
     
     public boolean removeUser(ObjectId id){
-        
+        try{
+            Document query = new Document("_id",id);
+            documentCollection.findOneAndDelete(query);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }      
     }
-    
+
     public boolean updateUser(User user){
-    
+        try{
+            Document query = new Document("_id",user.getId());
+            Document userDoc = new Document();
+            userDoc.append("_id", new ObjectId())
+                    .append("username", user.getUsername())
+                    .append("edad", user.getEdad())
+                    .append("shows", user.getShows());
+            documentCollection.findOneAndReplace(query, userDoc);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
     }
     
     public List<User> listAll(){
-    
+        return documentCollection.find().into(new ArrayList());
     }
     
     public List<User> listByShow(String show){
-    
-    }
-    
-    public List<User> listByName(String username){
-    
-    }
-    
-    public List<User> listByAge(int age){
-    
+        Bson filter = Filters.eq("shows.titulo", show);
+        return documentCollection.find(filter).into(new ArrayList());
     }
 }
